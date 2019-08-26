163c5f001e501bb26756bbb125264b8290d459d6
hoerzz <45986424+hoerzz@users.noreply.github.com>
2019-08-26 14:18:18 +0700

    Add files via upload

---
+++ liveCode.js
@@ -0,0 +1,41 @@
+//SOAL NO 1
+
+  STORE "NILA" 100;
+
+IF "NILAI" >= 80
+  DO "Mndapatkan Nilai A"
+ELSE IF "NILAI" >= 79
+  DO "Mendapatkan Nilai B"
+ELSE IF "NILAI" >= 64
+  DO "Mendapatkan Nilai C"
+ELSE IF "NILAI" >= 49
+  DO "Mendapatkan Nilai D"
+ELSE IF "NILAI" >= 34
+  DO "Mendapatkan Nilai E"
+ELSE
+  DO "Nilai Invalid"
+DISPLAY "NILAI"
+
+//SOAL NO 2
+
+var name = 'rizky';
+
+var grup = ["a", "b", "c", 'd']
+
+if (name == [0] ){
+  console.log ("Masuk Grup")
+}
+
+//SOAL NO 3
+
+function addTitle(person1,person2,person3,person4){
+
+}
+
+function changeSpaceWith(str,to){
+
+}
+
+function cutString (str){
+
+}
